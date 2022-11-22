25ac7c5e7f4f1f2117b74a0a98bcf1d579dbef22
Salomeja Plukaite <s.plukaite@gmail.com>
2022-11-22 17:04:24 +0200

    create frontend app

---
+++ src/main/js/projects-frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MenuAppBar from './components/AppBar';
+
+function App() {
+  return (
+    <div className="App">
+      <MenuAppBar/>
+    </div>
+  );
+}
+
+export default App;
